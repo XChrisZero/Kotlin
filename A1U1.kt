00f5cc81d3197bae9194b64655f13d3cf3b07e68
Christian Kuriak Chung Ferrari <161791400+XChrisZero@users.noreply.github.com>
2025-02-13 20:20:29 -0300

    Tipos de dados

---
+++ A1U1.kt
@@ -0,0 +1,18 @@
+fun main() {
+    // comentarios 
+    
+    /* comentarios 
+      multi linhas */
+    
+    //variaveis e constantes
+    var idade = 30 // criando uma variavel
+    val nome = 'Chris' //constante
+    
+    // tipos de dados
+    var inteiroA : Int = 10 // numero inteiro
+    var decimalA : Float = 10.5 // Numero Decimal 
+    var caractere : Character = 'A' //Letras
+    var textoA : String = 'Um texto' // Conjunto de letras
+    var boolA : booleam = true //binario
+    
+    //Inferencia de tipos
